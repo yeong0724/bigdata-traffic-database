eb2424cc09bf59c3738073e95a3a7fef59ef2115
KJY <kid4211@gmail.com>
2024-07-28 01:48:16 +0900

    Chapter 11. 댓글 조회 기능 개발 + Connection Pool 옵션 설정

--- backend/src/main/java/com/onion/backend/controller/ArticleController.java
+++ backend/src/main/java/com/onion/backend/controller/ArticleController.java
@@ -4,26 +4,31 @@ import com.onion.backend.dto.EditArticleDto;
 import com.onion.backend.dto.WriteArticleDto;
 import com.onion.backend.entity.Article;
 import com.onion.backend.service.ArticleService;
+import com.onion.backend.service.CommentService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/api/boards")
+@Tag(name = "ArticleController", description = "게시글 API")
 public class ArticleController {
-    private final AuthenticationManager authenticationManager;
-
     private final ArticleService articleService;
+    private final CommentService commentService;
 
-    public ArticleController(AuthenticationManager authenticationManager, ArticleService articleService) {
-        this.authenticationManager = authenticationManager;
+    @Autowired
+    public ArticleController(ArticleService articleService, CommentService commentService) {
         this.articleService = articleService;
+        this.commentService = commentService;
     }
 
+    @Operation(summary = "게시글 등록")
     @PostMapping("/{boardId}/articles")
     public ResponseEntity<Article> writeArticle(
             @RequestBody WriteArticleDto writeArticleDto,
@@ -35,6 +40,7 @@ public class ArticleController {
     }
 
     @GetMapping("/{boardId}/articles")
+    @Operation(summary = "게시글 조회")
     public ResponseEntity<List<Article>> getArticle(
             @Parameter(description = "Board 테이블 Key 값", example = "1", required = true) @PathVariable Long boardId,
             @RequestParam(required = false) Long lastId,
@@ -51,6 +57,17 @@ public class ArticleController {
         return ResponseEntity.ok(articleService.firstGetArticle(boardId));
     }
 
+    @GetMapping("/{boardId}/articles/{articleId}")
+    @Operation(summary = "게시글 조회(+ 댓글)")
+    public ResponseEntity<Article> getArticleWithComment(
+            @Parameter(description = "Board 테이블 Key 값", example = "1", required = true) @PathVariable Long boardId,
+            @Parameter(description = "Article 테이블 Key 값", example = "1", required = true) @PathVariable Long articleId
+    ) {
+        CompletableFuture<Article> article = commentService.getArticleWithComment(boardId, articleId);
+        return ResponseEntity.ok(article.resultNow());
+    }
+
+    @Operation(summary = "게시글 수정")
     @PutMapping("/{boardId}/articles/{articleId}")
     public ResponseEntity<Article> editArticle(
             @Parameter(description = "Board 테이블 Key 값", example = "1", required = true) @PathVariable Long boardId,
@@ -60,6 +77,7 @@ public class ArticleController {
         return ResponseEntity.ok(articleService.editArticle(boardId, articleId, editArticleDto));
     }
 
+    @Operation(summary = "게시글 삭제")
     @DeleteMapping("/{boardId}/articles/{articleId}")
     public ResponseEntity<String> deleteArticle(
             @Parameter(description = "Board 테이블 Key 값", example = "1", required = true) @PathVariable Long boardId,
--- backend/src/main/java/com/onion/backend/service/CommentService.java
+++ backend/src/main/java/com/onion/backend/service/CommentService.java
@@ -1,8 +1,6 @@
 package com.onion.backend.service;
 
 import com.onion.backend.common.CommonUtil;
-import com.onion.backend.dto.EditArticleDto;
-import com.onion.backend.dto.WriteArticleDto;
 import com.onion.backend.dto.WriteCommentDto;
 import com.onion.backend.entity.Article;
 import com.onion.backend.entity.Board;
@@ -17,17 +15,16 @@ import com.onion.backend.repository.CommentRepository;
 import com.onion.backend.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
-
+import org.springframework.scheduling.annotation.Async;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 @Service
 public class CommentService {
@@ -109,5 +106,41 @@ public class CommentService {
         Duration duration = Duration.between(localDateTime, dateAsLocalDateTime);
 
         return Math.abs(duration.toMinutes()) > 1;
+    }
+
+    @Async
+    protected CompletableFuture<Article> getArticle(Long boardId, Long articleId) {
+        Optional<Board> board = boardRepository.findById(boardId);
+        if (board.isEmpty()) {
+            throw new ResourceNotFoundException("board not found");
+        }
+        Optional<Article> article = articleRepository.findById(articleId);
+        if (article.isEmpty() || article.get().getIsDeleted()) {
+            throw new ResourceNotFoundException("article not found");
+        }
+        return CompletableFuture.completedFuture(article.get());
+    }
+
+    @Async
+    protected CompletableFuture<List<Comment>> getComments(Long articleId) {
+        return CompletableFuture.completedFuture(commentRepository.findByArticleId(articleId));
+    }
+
+    public CompletableFuture<Article> getArticleWithComment(Long boardId, Long articleId) {
+        CompletableFuture<Article> articleFuture = this.getArticle(boardId, articleId);
+        CompletableFuture<List<Comment>> commentsFuture = this.getComments(articleId);
+
+        return CompletableFuture.allOf(articleFuture, commentsFuture)
+                .thenApply(voidResult -> {
+                    try {
+                        Article article = articleFuture.get();
+                        List<Comment> comments = commentsFuture.get();
+                        article.setComments(comments);
+                        return article;
+                    } catch (InterruptedException | ExecutionException e) {
+                        e.printStackTrace();
+                        return null;
+                    }
+                });
     }
 }
